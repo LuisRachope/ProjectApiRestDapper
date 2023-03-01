a9bd4057272a20bcff19ddd0acb337d5cb6c98ad
Luis Rachope <luis.chiasso@hotmail.com>
2023-03-01 20:34:30 -0300

    Add project files.

---
+++ ProjectApiRestDapper/Repository/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectApiRestDapper.Repository.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Task<List<>>
+
+    }
+}
