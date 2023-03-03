96e36d5627c9c6c488239842c108a95702f06aab
Luis Rachope <luis.chiasso@hotmail.com>
2023-03-02 21:33:43 -0300

    Criado o DbContext e o Services para adicioanr ao escopo do projeto

--- ProjectApiRestDapper/Context/DbContext.cs
+++ ProjectApiRestDapper/Context/DbContext.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace ProjectApiRestDapper.Context
 {
-    public class DbContext
+    public class DbContext : IAsyncDisposable
     {
+        public MySqlConnection Connection { get; }
+
+        public DbContext(string connection)
+        {
+            Connection = new MySqlConnection(connection);
+            Connection.OpenAsync();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await Connection.CloseAsync();
+            await Connection.ClearAllPoolsAsync();
+            await Connection.DisposeAsync();
+        }
     }
 }
---
+++ ProjectApiRestDapper/Extensions/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectApiRestDapper.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectApiRestDapper.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void AddContexts(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<DbContext, DbContext>(provider => 
+                new DbContext(configuration["ConnectionString:SecretNameDbContext"])
+            );
+        }
+
+    }
+}
